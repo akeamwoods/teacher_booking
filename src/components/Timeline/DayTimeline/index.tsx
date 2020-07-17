0c1432d1975b281d95080ca60dace5975df7ba4c
akeamwoods <a19941@hotmail.co.uk>
2020-07-17 21:47:52 +0100

    changed day colours

--- src/components/Timeline/DayTimeline/index.tsx
+++ src/components/Timeline/DayTimeline/index.tsx
@@ -5,6 +5,7 @@ import {
   addDays,
   isSameDay,
   format,
+  getDay,
 } from "date-fns";
 import {
   DayTimelineWrapper,
@@ -26,15 +27,15 @@ export const DayTimeline: React.FC<{ selectedDate: Date }> = ({
   return (
     <DayTimelineWrapper>
       {days.map((day) => (
-        <Day day={day} currentDay={isSameDay(selectedDate, day)} />
+        <Day day={day} isCurrentDay={isSameDay(selectedDate, day)} />
       ))}
     </DayTimelineWrapper>
   );
 };
 
-export const Day: React.FC<{ day: Date; currentDay: boolean }> = ({
+export const Day: React.FC<{ day: Date; isCurrentDay: boolean }> = ({
   day,
-  currentDay,
+  isCurrentDay,
 }) => {
   const dispatch = useDispatch();
   const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
@@ -45,12 +46,34 @@ export const Day: React.FC<{ day: Date; currentDay: boolean }> = ({
       inline: "center",
     });
   };
+  const getDayColour = (day: Date): string => {
+    const dayAsNumber = getDay(day);
+    //is mon/wed/fri
+    if ([1, 3, 5].includes(dayAsNumber)) {
+      return "#fdf6ec";
+    } else if ([2, 4].includes(dayAsNumber)) {
+      return "#ffce85";
+    } else {
+      return `repeating-linear-gradient(
+        135deg,
+        #f8f8f7,
+        #f8f8f7 2px,
+        #efefef 2px,
+        #efefef 4px
+      )`;
+    }
+  };
   return (
     <DayWrapper
       onClick={handleClick}
-      style={{ borderBottomColor: currentDay ? "#000" : "#e1e1e1" }}
+      style={{ borderBottomColor: isCurrentDay ? "#000" : "#e1e1e1" }}
     >
-      <DayContainer style={{ background: currentDay ? "gold" : "#e1e1e1" }}>
+      <DayContainer
+        style={{
+          background: isCurrentDay ? "#000" : getDayColour(day),
+          color: isCurrentDay ? "#fff" : "#000",
+        }}
+      >
         {format(day, "d")}
       </DayContainer>
       <DayText>{format(day, "E")}</DayText>
