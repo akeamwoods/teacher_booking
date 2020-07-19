98cb04ff6b19288fa6834c4fe5859bbb0c55261e
akeamwoods <a19941@hotmail.co.uk>
2020-07-19 01:30:39 +0100

    renamed axis

--- src/components/Axis/index.tsx
+++ src/components/Axis/index.tsx
@@ -1,107 +1,40 @@
 import React from "react";
-import prettyTicks from "./PrettyTicks";
-import { ScaleLinear, ScaleTime } from "d3-scale";
-import styled from "styled-components";
-import { format } from "date-fns";
 
-type Props = {
-  colour?: string;
-  isVisible?: boolean;
-  integerScale?: boolean;
+import { format } from "date-fns";
+import { ScaleLinear, ScaleTime } from "d3";
+import { Wrapper, Tick } from "./style";
+
+export const Axis: React.FC<{
   scale: ScaleLinear<number, number> | ScaleTime<number, number>;
   tickWidth?: number;
-  width?: number;
-  suffix?: string;
-  /** Optional tooltip text; use "{T}" as placeholder for tick value */
-  title?: string;
-  unitTitle?: string;
-};
-
-const defaultProps = {
-  colour: "#FFF",
-  isVisible: true,
-  integerScale: false,
-  tickWidth: 30,
-  width: 40,
-  unitTitle: "",
-};
-
-export default function Axis(props: Props) {
-  const {
-    colour,
-    isVisible,
-    integerScale,
-    scale,
-    tickWidth,
-    width,
-    suffix,
-    title,
-    unitTitle,
-  } = {
-    ...defaultProps,
-    ...props,
-  };
-
+}> = ({ scale, tickWidth = 30 }) => {
   const height = scale.range()[0];
 
+  const ticks = (
+    scale: ScaleLinear<number, number> | ScaleTime<number, number>,
+    width: number,
+    tickWidth = 100
+  ) => {
+    try {
+      const maxNumber = Math.max(Math.floor(width / tickWidth), 1);
+      const ticks: any = scale.ticks(maxNumber);
+      return ticks.length < maxNumber
+        ? ticks
+        : ticks.filter((_: any, i: number) => i % 2 === 0);
+    } catch (error) {
+      return [];
+    }
+  };
   return (
-    <Container visible={isVisible} width={width} height={height}>
-      <UnitText width={width} colour={colour}>
-        {unitTitle}
-      </UnitText>
-      {prettyTicks(scale, height, tickWidth).map((tick: Date) => (
+    <Wrapper>
+      {ticks(scale, height, tickWidth).map((tick: Date) => (
         <Tick
           key={tick.toISOString()}
-          colour={colour}
-          transform={`translate(-50%, ${scale(tick).toFixed(0)}px)`}
-          title={(title
-            ? title.replace("{T}", `${tick.toString()} ${unitTitle ?? ""}`)
-            : `${tick.toISOString()} ${unitTitle ?? ""}`
-          ).trim()}
+          transform={`translateY(${scale(tick).toFixed(0)}px)`}
         >
           {format(tick, "kk:mm")}
-          {suffix}
         </Tick>
       ))}
-    </Container>
+    </Wrapper>
   );
-}
-
-const Container = styled.div<{
-  visible: boolean;
-  height: number;
-  width: number;
-}>`
-  position: relative;
-  display: ${(p) => (p.visible ? "block" : "none")};
-  height: ${(p) => p.height}px;
-  width: ${(p) => p.width}px;
-  flex: ${(p) => `0 0 ${p.width}px`};
-  text-align: center;
-  cursor: ns-resize;
-`;
-
-const Tick = styled.div.attrs<{
-  colour: string;
-  transform: string;
-}>((props) => ({
-  style: { transform: props.transform },
-}))<{ colour: string; transform: string }>`
-  position: absolute;
-  line-height: 0; /* Line up the *middle* of the number, not its baseline, with its value */
-  left: 50%;
-  font-size: 0.7em;
-  font-weight: 800;
-  color: #abb1b6;
-`;
-
-const UnitText = styled.div<{ width: number; colour: string }>`
-  position: absolute;
-  text-align: center;
-  width: ${(p) => p.width}px;
-  color: ${(p) => p.colour};
-  top: -2.1em;
-  cursor: default;
-  white-space: nowrap;
-  overflow: hidden;
-`;
+};
--- src/components/Schedule/index.tsx
+++ src/components/Schedule/index.tsx
@@ -1,25 +1,22 @@
 import React from "react";
 import { Wrapper } from "./style";
 import { useTypedSelector } from "../../store";
-import Axis from "../Axis";
 import { startOfDay, endOfDay } from "date-fns";
-import { scaleTime, scaleLinear } from "d3";
-import { NewAxis } from "../NewAxis";
+import { scaleTime } from "d3";
+import { Axis } from "../Axis";
 
 export const Schedule = () => {
   const lessons = useTypedSelector(
     (state) => state.lessons[state.selectedDate]
   );
 
-  const scale1 = scaleTime()
-    // .range([0, 500])
+  const scale = scaleTime()
     .range([400, 0])
     .domain([endOfDay(new Date()), startOfDay(new Date())]);
 
-  const scale2 = scaleLinear().range([100, 0]).domain([0, 100]).nice();
   return (
     <Wrapper>
-      <NewAxis scale={scale1} />
+      <Axis scale={scale} />
       {lessons &&
         lessons.map((lesson) => <div key={lesson.id}>{lesson.subject}</div>)}
     </Wrapper>
