d73b2631f89fa59eed29e3eb16041c89856d7fec
akeamwoods <a19941@hotmail.co.uk>
2020-07-19 20:48:58 +0100

    separated date picker logic

---
+++ src/components/DatePicker/style.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  padding: 10px;
+  background: #fff;
+  border: 1px solid #edebe9;
+  svg {
+    margin-left: 10px;
+  }
+`;
--- src/components/LessonForm/index.tsx
+++ src/components/LessonForm/index.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { actions } from "../../store/actions";
 import { Lesson } from "../../store/types";
 import { v4 as uuidv4 } from "uuid";
-import { subHours } from "date-fns";
+import { useTypedSelector } from "../../store";
+import { DatePicker } from "../DatePicker";
 
 export const LessonForm = () => {
+  const currentDate = useTypedSelector((state) => state.selectedDate);
   const dispatch = useDispatch();
   const start = "2020-07-19T14:45:00.000Z";
   const end = "2020-07-19T15:45:00.000Z";
+  const [date, setDate] = useState(undefined as undefined | Date);
   return (
     <div
       style={{
@@ -34,8 +37,7 @@ export const LessonForm = () => {
           );
         }}
       >
-        <input></input>
-        <button>Add</button>
+        <DatePicker selectedDate={date ? date : new Date(currentDate)} />
       </form>
     </div>
   );
