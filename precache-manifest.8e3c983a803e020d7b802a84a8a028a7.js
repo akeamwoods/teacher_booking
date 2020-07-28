da9126cd054e8297b12e6421d21c7f6b182efef7
akeamwoods <a19941@hotmail.co.uk>
2020-07-28 18:05:30 +0100

    Updates

--- precache-manifest.8e3c983a803e020d7b802a84a8a028a7.js
+++ precache-manifest.8e3c983a803e020d7b802a84a8a028a7.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2d825a86521a7c8917bf3ca40cff42b4",
+    "revision": "94b696ee6b4ff3ac1ec0882129d1013e",
     "url": "/teacher_booking/index.html"
   },
   {
-    "revision": "3a26819ef976db9af2f3",
+    "revision": "6a80c2edd30c8e7b7f21",
     "url": "/teacher_booking/static/css/main.167331c4.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/teacher_booking/static/js/2.9077f41f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3a26819ef976db9af2f3",
-    "url": "/teacher_booking/static/js/main.f4888a41.chunk.js"
+    "revision": "6a80c2edd30c8e7b7f21",
+    "url": "/teacher_booking/static/js/main.129e7b7b.chunk.js"
   },
   {
     "revision": "27b6ae749ec4008e89b0",
